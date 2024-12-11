4607c30ae6d0fbc5de118cd32be21c0ab903cc2c
MedjidovTarlan <medjidovtarlan29@gmail.com>
2024-12-11 18:41:30 +0300

    Add files via upload

---
+++ peremennye.py
@@ -0,0 +1,7 @@
+first_name= 'Python,'
+second_name ='12,'
+fird_name= '1.5,'
+fourth_name= '0.125 часа.'
+print ('Курс:',first_name, 'всего задач:', second_name, 'затрачено часов:',fird_name, 'среднее время выполнения', fourth_name  )
+
+
